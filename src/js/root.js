5ff5fface0065a708f23ef7ff35f4430902a53ee
Egor Pavlov <73013930+pavlovsun@users.noreply.github.com>
2022-01-19 16:22:14 +1000

    Delete src directory

--- src/js/root.js
+++
@@ -1,3 +0,0 @@
-const ROOT_HEADER = document.getElementById('headerMain');
-const ROOT_FOOTER = document.getElementById('footerMain');
-const ROOT_DESCRIPTION = document.getElementById('description');
